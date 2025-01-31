5a11522d96166d364ef343d2f2ec2d5e8eaf021b
anarehder <anapaula.rehder@gmail.com>
2025-01-31 19:27:31 +0000

    feat: user portal page em branco

---
+++ src/pages/UserPortalPage.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+function UserPortalPage() {
+
+    return (
+        <PageContainer>
+            <div> PÁGINA DO USUÁRIO </div>
+        </PageContainer>
+    )
+}
+
+export default UserPortalPage;
+
+
+const PageContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    position: relative;
+`
