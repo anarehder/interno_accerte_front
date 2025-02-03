0a6eec59c25a90f08d1e71561140346840fec7cb
anarehder <anapaula.rehder@gmail.com>
2025-02-03 17:59:41 +0000

    feat: user portal page estático

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styled from 'styled-components'
 import HomePage from "./pages/HomePage";
-import UserPortalPage from "./pages/EquipmentsPage";
-import EquipmentsPage from "./pages/EquipmentsPage";
 import LoginPage from "./pages/LoginPage";
+import EquipmentsPage from "./pages/EquipmentsPage";
+import UserPortalPage from "./pages/UserPortalPage";
 
 function App() {
 
---
+++ src/pages/UserPortalPage.jsx
@@ -0,0 +1,138 @@
+import styled from 'styled-components';
+import backgroundImage from "/home/accerte/interno_accerte_front/src/assets/FUNDO_1.jpg"; // Importação da imagem
+import { TbCertificate } from "react-icons/tb";
+import { PiAirplaneTiltBold, PiSignature } from "react-icons/pi";
+import { FaComputer } from "react-icons/fa6";
+
+function UserPortalPage() {
+
+    return (
+        <PageContainer>
+            <TitleContainer>
+                BEM-VINDO AO SEU 
+                <div>
+                PORTAL ACCERTE
+                </div>
+            </TitleContainer>
+            <CardsContainer>
+                <Card>
+                    <TbCertificate size={60} />
+                    <div>CERTIFICAÇÕES</div>
+                    <button>SABER MAIS</button>
+                </Card>
+                <Card>
+                    <PiAirplaneTiltBold size={60} />
+                    <div>FÉRIAS</div>
+                    <button>SABER MAIS</button>
+                </Card>
+                <Card>
+                    <FaComputer size={60} />
+                    <div>EQUIPAMENTOS</div>
+                    <button>SABER MAIS</button>
+                </Card>
+                <Card>
+                    <PiSignature size={60} />
+                    <div>
+                        <div>
+                            ASSINATURA
+                        </div>
+                        <div>
+                            DE E-MAIL
+                        </div>
+                    </div>
+                    <button>SABER MAIS</button>
+                </Card>
+            </CardsContainer>
+        </PageContainer>
+    )
+}
+
+export default UserPortalPage;
+
+
+const PageContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    flex-direction: column;
+    background-image: url(${backgroundImage});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    align-items: center;
+    gap: 50px;
+    background-color: rgba(0, 0, 0, 0.2);
+`
+
+const TitleContainer = styled.div`
+    width: 100%;
+    gap: 15px;
+    background-color: rgba(0, 0, 0, 0.4);
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 25vh;
+    color: white;
+    font-family: "Lato", serif;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 50px;
+    div {
+        font-size: 80px;
+        justify-content: center;
+        font-family: "Dosis", serif;
+        word-spacing: 20px;
+    }
+}
+`
+
+const CardsContainer = styled.div`
+    display: flex;
+    width: 80vw;
+    justify-content: center;
+    gap: 4%;
+    padding: 40px;
+`;
+
+const Card = styled.div`
+    width: 15%;
+    height: 30vh;
+    color: white;
+    border-radius: 15px;
+    background: linear-gradient(25deg,rgb(37, 48, 57),rgb(9, 18, 32));
+    box-shadow: 
+        10px 10px 20px rgba(0, 0, 0, 0.5),
+        -10px -10px 20px rgba(50, 47, 47, 0.1);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+    position: relative;
+    overflow: hidden;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+        box-shadow: 
+            15px 15px 25px rgba(0, 0, 0, 0.2),
+            -15px -15px 25px rgba(255, 255, 255, 0.2);
+        transform: translateY(-5px);
+    }
+
+    div{
+        font-family: "Marcellus", serif;
+        font-size: 24px;
+        word-break: break-word;
+        font-weight: 600;
+        width: 95%;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+    }
+
+    button {
+        font-family: "Lato", serif;
+        fof-size:15px;
+        background-color:rgb(8, 15, 27);
+    }
+`;
