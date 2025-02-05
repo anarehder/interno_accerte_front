aacb3b340d23ee504c4c7e113f89af913d66d110
anarehder <anapaula.rehder@gmail.com>
2025-02-05 20:14:52 +0000

    fix: altura minima pagina assinatura

--- src/pages/SignatureEmailPage.jsx
+++ src/pages/SignatureEmailPage.jsx
@@ -185,6 +185,7 @@ export default SignatureEmailPage;
 const PageContainer = styled.div`
     width: 100%;
     height: 100%;
+    min-height: 100vh;
     flex-direction: column;
     align-items: center;
     gap: 30px;
