ff772339f53b39aabadd2af8b2bb3efb3367ceb4
anarehder <anapaula.rehder@gmail.com>
2025-05-22 12:45:03 +0000

    feat: novos fundos de tela

--- src/pages/FastLinksPage.jsx
+++ src/pages/FastLinksPage.jsx
@@ -36,16 +36,16 @@ const FastLinksPage = () => {{
             {Apple: "https://apps.apple.com/br/app/wellhub-gympass/id703761434" },
             {Android: "https://play.google.com/store/apps/details?id=com.gympass&hl=pt_BR" }
         ]},
-        {Clicksign: "https://app.clicksign.com/accounts/13505/tracking/notifications"},
         {MarqPonto: [
             {Apple: "https://apps.apple.com/br/app/marqponto/id1379890385" },
             {Android: "https://play.google.com/store/apps/details?id=br.com.marqponto" }
         ]},
-        {GymRats: "https://share.gymrats.app/join?code=ITGCHBMV"},
         {Onfly: [
             {Apple: "https://apps.apple.com/br/app/onfly/id6443720203?l=en-GB" },
             {Android: "https://play.google.com/store/apps/details?id=com.onfly.app" }
         ]},
+        {Clicksign: "https://app.clicksign.com/accounts/13505/tracking/notifications"},
+        {GymRats: "https://share.gymrats.app/join?code=ITGCHBMV"},
     ];
 
     useEffect(() => {
@@ -89,17 +89,6 @@ const FastLinksPage = () => {{
                 dados &&
                 <>
                     <List>
-                        {fixedLinks.map((linkObj, ind) => (
-                            <Card key={Object.keys(linkObj) || ind}>
-                                <Info><img src={imageMap[Object.keys(linkObj)]} alt={Object.keys(linkObj)} /> </Info>
-                                {Object.values(linkObj).map((links, index) => (
-                                    <>
-                                        {renderLinks(links, index)}
-                                    </>
-                                ))}
-                            </Card>
-                        ))}
-                        
                         <Card >
                             <Info>Fundos de Tela</Info>
                             <InfoButton>
@@ -110,15 +99,31 @@ const FastLinksPage = () => {{
                         </Card>
                         {wallpaper &&
                             <SmallList>
-                                {dados?.background?.map((file, index) => (
-                                    <Card key={index}>
-                                        <Info>{file.name.slice(0, -4)}</Info>
-                                        <InfoButton><button><a href={file.url} target="_blank"> Acessar </a></button></InfoButton>
-                                    </Card>
-                                ))}
+                                <PostsContainer>
+                                    <div className="carousel" >
+                                        {dados?.background?.map((p, index) => (
+                                            <div className="card" key={index} >
+                                                <a href={p.url} target="_blank">
+                                                    <img src={p.url} alt={"Post do Linkedin"} />
+                                                </a>
+                                            </div>
+                                        ))}
+                                    </div>
+                                    
+                                </PostsContainer>
+                                <p>clique para acessar a imagem</p>
                             </SmallList>
                         }
-
+                        {fixedLinks.map((linkObj, ind) => (
+                            <Card key={Object.keys(linkObj) || ind}>
+                                <Info><img src={imageMap[Object.keys(linkObj)]} alt={Object.keys(linkObj)} /> </Info>
+                                {Object.values(linkObj).map((links, index) => (
+                                    <>
+                                        {renderLinks(links, index)}
+                                    </>
+                                ))}
+                            </Card>
+                        ))}
                         <Card >
                             <Info>Documentos Padrão</Info>
                             <InfoButton>
@@ -165,7 +170,7 @@ const List = styled.div`
     margin: 30px 0 0 0;
     justify-content: center;
     align-items: center;
-    width: 60%;
+    width: 70%;
 `;
 
 const SmallList = styled.div`
@@ -174,9 +179,90 @@ const SmallList = styled.div`
     gap: 10px;
     justify-content: center;
     align-items: center;
-    width: 90%;
+    width: 95%;
     margin-bottom: 30px;
 `;
+
+
+const PostsContainer = styled.div`
+    .carousel {
+        display: flex;
+        gap: 20px;
+        overflow-x: auto; /* Ativa o scroll horizontal */
+        overflow-y: hidden; /* Evita scroll vertical */
+        height: 230px;
+        padding-left: 10px;
+        align-items: center;
+    }
+        /* Para navegadores baseados em WebKit (Chrome, Safari, Edge) */
+    .carousel::-webkit-scrollbar {
+        height: 6px; /* Altura da barra de rolagem */
+    }
+
+    .carousel::-webkit-scrollbar-track {
+        background: transparent; /* Fundo da barra */
+    }
+
+    .carousel::-webkit-scrollbar-thumb {
+        background: gray; /* Cor da barra de rolagem */
+        border-radius: 10px; /* Bordas arredondadas */
+    }
+
+    .carousel::-webkit-scrollbar-thumb:hover {
+        background: darkgray; /* Cor ao passar o mouse */
+    }
+
+  .card {
+    flex: 0 0 260px;
+    background: white;
+    border-radius: 35px;
+    overflow: hidden;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    transition: transform 0.3s ease-in-out;
+    width: 280px;
+    margin: 5px 0;
+    height: 160px;
+    position: relative;
+    display: inline-block;
+    .card .link-overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 1; /* Garante que o link fique sobre a imagem */
+        opacity: 0; /* Torna o link invisível */
+        background-color: rgba(0, 0, 0, 0); /* Transparente, mas garante que o link será clicável */
+    }
+
+    &:hover {
+      transform: scale(1.05);
+      border-radius: 35px;
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center; 
+    }
+
+    .content {
+      padding: 10px;
+      text-align: center;
+      
+      h3 {
+        font-size: 18px;
+        color: #222;
+        margin-bottom: 10px;
+      }
+
+      p {
+        font-size: 14px;
+        color: #555;
+      }
+    }
+`
 
 const Card = styled.div`
     background: white;
